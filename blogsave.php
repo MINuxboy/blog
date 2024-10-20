9efbd40795d28a946e5f97d9f7441851ada7509e
MINuxboy <minulapriyashan2004@gmail.com>
2024-10-20 08:53:37 +0530

    execute data

--- blogsave.php
+++ blogsave.php
@@ -7,11 +7,15 @@ print_r($vals);
 $myclass = new sqlobj($bdd);  
 
 $str1 = "SELECT * FROM posts";
-
+// var_dump($vals);
+// die();
 $stmt= $bdd->prepare("INSERT INTO `posts` (`title`, `content`, `creator`) VALUES (:title, :content, '1')");
-$stmt->bindParam(':title',$vals);
-$stmt->bindParam(':content',$vals);
+// $stmt->bindParam(':title',$vals[0]);
+// $stmt->bindParam(':content',$vals[1]);
 
-$stmt->execute();
+$stmt->execute([
+    ":title" => $vals[0],
+    ":content" => $vals[1]
+]);
 echo "complete";
 ?>
